acdcc5ee03c762abc6213d0480f3004c38885c30
softteco-kt <121219358+softteco-kt@users.noreply.github.com>
2022-12-23 19:15:52 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,12 +13,10 @@ echo "Downloading ChromeDriver..."
 
 if [[ "$OSTYPE" =~ ^msys ]] ; then
     curl -LO https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_win32.zip
-
 fi
 
 if [[ "$OSTYPE" =~ ^linux ]]; then
     wget https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip
-    export .env
 fi
 
 unzip *.zip -d .
