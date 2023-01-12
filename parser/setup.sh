7b2c77eff5ad2e45c12ff255f1d54e937566dba7
softteco-kt <121219358+softteco-kt@users.noreply.github.com>
2023-01-12 16:34:32 +0000

    optimizations and cleanup

--- parser/setup.sh
+++ parser/setup.sh
@@ -4,16 +4,24 @@ then
     mkdir data
 fi
 
+# Chrome versions are available at https://www.ubuntuupdates.org/package/google_chrome/stable/main/base/google-chrome-stable?id=202706&page=1
+# Chromedriver versions are available at https://chromedriver.chromium.org/downloads
+
+export CHROMEDRIVER_VERSION=108.0.5359.71
+export CHROME_VERSION=$CHROMEDRIVER_VERSION-1
+
 echo "Downloading ChromeDriver..."
 
-
 if [[ "$OSTYPE" =~ ^msys ]] ; then
-    curl -LO https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_win32.zip
+    curl -LO https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_win32.zip
 fi
 
 if [[ "$OSTYPE" =~ ^linux ]]; then
-    wget https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip
+    wget https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
 fi
 
-unzip *.zip -d .
-rm *.zip
+unzip *.zip -d . && rm *.zip
+
+python3 -m venv .venv && \
+    source .venv/bin/activate && \
+    pip3 install --no-cache-dir -r requirements.txt
