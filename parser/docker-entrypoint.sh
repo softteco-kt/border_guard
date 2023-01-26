80d06135f784f188642a802d19179802182074c7
softteco-kt <k.turaev@softteco.com>
2023-01-26 20:44:15 +0500

    parser log to stdout

--- parser/docker-entrypoint.sh
+++ parser/docker-entrypoint.sh
@@ -6,5 +6,8 @@ sh setup_cron.sh
 # Create tables
 python3 models.py
 
+# Redirecting output of non pid 1 process to docker logs
+tail -n 0 -q -F /usr/src/parser/*.log >> /proc/1/fd/1 &
+
 # Run cron as main process
 cron -f
