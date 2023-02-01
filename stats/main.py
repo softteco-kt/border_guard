d454bacc690234be4ce699ce13df36b973d6eb55
softteco-kt <k.turaev@softteco.com>
2023-02-01 18:59:57 +0500

    data timeframes, tooltips, labels

--- stats/main.py
+++ stats/main.py
@@ -1,19 +1,45 @@
 import requests
-import streamlit as st
+import enum
 
+import streamlit as st
+import altair as alt
 import pandas as pd
 import numpy as np
 
-import altair as alt
-import json
-from datetime import datetime as dt, timedelta as td
+from datetime import datetime as dt
+from datetime import timedelta as td
+
+
+class Timeframe(enum.StrEnum):
+    """Data timeframes"""
+
+    min = "Min"
+    hourly = "Hourly"
+    daily = "Daily"
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+
+class Columns(enum.StrEnum):
+    """DataFrame column names"""
+
+    moving_average = "Moving Average"
+    cars = "Number of cars"
+    time = "Datetime"
 
 
 def date_to_timestamp(date):
     return int(dt.combine(date, dt.min.time()).timestamp())
 
 
-def get_data(date_from=None, date_to=None):
+def get_data(
+    timeframe: str,
+    moving_average: int = 1,
+    date_from: dt.date = None,
+    date_to: dt.date = None,
+):
     response = requests.get(
         "http://api:8000/cars_on_border",
         params={
@@ -23,60 +49,103 @@ def get_data(date_from=None, date_to=None):
         },
     )
     data = pd.DataFrame(response.json(), columns=["created_at", "number_of_cars"])
+
+    data.rename(
+        columns={"created_at": Columns.time, "number_of_cars": Columns.cars},
+        inplace=True,
+    )
+
+    # Convert to Datetime
+    data[Columns.time] = pd.to_datetime(data[Columns.time])
+
+    # Transform data to selected timeperiod
+    match timeframe:
+        case Timeframe.min:
+            # Additional column for tooltip
+            data["Time"] = data[Columns.time].dt.strftime("%b %-d %H:%M")
+        case Timeframe.hourly:
+            data = data.resample("60min", on=Columns.time).mean()
+            data.reset_index(inplace=True)
+            data["Time"] = data[Columns.time].dt.strftime("%b %-d %H:00")
+        case Timeframe.daily:
+            data = data.resample("1d", on=Columns.time).mean()
+            data.reset_index(inplace=True)
+            data["Day"] = data[Columns.time].dt.strftime("%B %-d")
+
+    # Calculate moving average
+    data[Columns.moving_average] = (
+        data[Columns.cars].rolling(window=moving_average).mean().fillna(0)
+    )
+
+    # Round DataFrame with precision to 1 after comma
+    data = data.round(1)
+
     return data
 
 
-def draw_altair(data):
+def draw_altair(data: pd.DataFrame, timeframe: str):
+    tooltip = [Columns.cars]
+
+    x_axis_label = None
+
+    # Make appropriate to timeframe x-axis labels
+    match timeframe:
+        case Timeframe.min:
+            tooltip.append("Time")
+            x_axis_label = "yearmonthdatehoursminutes"
+        case Timeframe.hourly:
+            tooltip.append("Time")
+            x_axis_label = "yearmonthdatehours"
+        case Timeframe.daily:
+            tooltip.append("Day")
+            x_axis_label = "yearmonthdate"
+
+    # Chart configs on given data
     base = alt.Chart(data).encode(
-        x=alt.X("created_at", title="Datetime", type="temporal"),
-        y=alt.Y("number_of_cars", title="Number of Cars", type="quantitative"),
-        tooltip=["Time", "number_of_cars"],
+        x=alt.X(Columns.time, title="Datetime", type="temporal", timeUnit=x_axis_label),
+        y=alt.Y(Columns.cars, title="Number of Cars", type="quantitative"),
+        tooltip=tooltip,
     )
 
-    chart = (
-        alt.layer(
-            base.mark_point(),
-            base.mark_line().encode(
-                y=alt.Y(MOVING_AVERAGE_COL, type="quantitative"),
-                color=alt.value("#FFAA00"),
-            )
-            # .mark_rule(),
-        )
-        # .interactive()
+    # Draw scatter plot and line chart with base chart data
+    chart = alt.layer(
+        base.mark_point(),
+        base.mark_line().encode(
+            y=alt.Y(Columns.moving_average, type="quantitative"),
+            color=alt.value("#FFAA00"),
+        ),
     )
 
     return chart
 
 
-st.set_page_config(
-    layout="wide",
-    page_title="Border Guard",
-    page_icon="🧊",
-    menu_items={},
-)
+st.set_page_config(layout="wide", page_title="Border Guard", page_icon="🧊")
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.title("Statistics")
 
 # Input for api filtering
-ma_widget, from_widget, to_widget = st.columns(3)
-input_ma = ma_widget.number_input("Moving average (Hours)", 1) * 3
+tf_widget, ma_widget, from_widget, to_widget = st.columns(4)
+input_tf = tf_widget.selectbox("Timeframe:", Timeframe.list())
+input_ma = ma_widget.number_input("Moving average:", 1)
 input_date_from = from_widget.date_input("Date From:", dt.today() - td(days=1))
-input_date_to = to_widget.date_input("Date to: ")
+input_date_to = to_widget.date_input("Date To:")
 
-with st.spinner(text="Fetching data..."):
-    # Get API data
-    data = get_data(date_from=input_date_from, date_to=input_date_to)
 
 try:
-    # Calculate moving average
-    MOVING_AVERAGE_COL = f"Moving Average - {input_ma//3} hours"
-    data[MOVING_AVERAGE_COL] = data.rolling(window=input_ma).mean().fillna(0)
-
-    # Get datetime
-    times = pd.to_datetime(data.created_at)
-    data["Date"] = times.dt.date
-    data["Time"] = times.dt.strftime("%H:%M")
-
-    st.altair_chart(draw_altair(data), use_container_width=True)
-except:
+    # Get API data
+    data = get_data(
+        timeframe=input_tf,
+        moving_average=input_ma,
+        date_from=input_date_from,
+        date_to=input_date_to,
+    )
+    st.altair_chart(draw_altair(data, timeframe=input_tf), use_container_width=True)
+except Exception as e:
     st.write("No data...")
