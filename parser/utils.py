4f833b556ec3395fc74524057aa988d8b0e72314
softteco-kt <k.turaev@softteco.com>
2023-02-13 13:30:57 +0500

    image processing utils

--- parser/utils.py
+++ parser/utils.py
@@ -1,5 +1,9 @@
 from models import BorderCapture, database
 from send_msg import send_to_qu
+from PIL import Image
+
+import requests
+import io
 import os
 
 
@@ -38,3 +42,38 @@ def process_unprocessed_images():
         for image_id in unprocessed_images:
             # Send to queue for further processing
             send_to_qu(str(image_id))
+
+
+def is_black(img: Image, threshold=0.99):
+
+    # check if img is a url
+    if img.startswith("http"):
+        img_bytes = requests.get(img, stream=True).raw
+        img = Image.open(io.BytesIO(img_bytes))
+
+    try:
+        # convert to grayscale
+        pixels = img.convert("L").getdata()  # get the pixels as a flattened sequence
+    except Exception as ex:
+        print(ex)
+        raise Exception("Custom exception: Image is not valid. Perhaps url is broken?")
+
+    nblack, black_thresh = 0, 50
+
+    # count black pixels in image
+    for pixel in pixels:
+        if pixel < black_thresh:
+            nblack += 1
+
+    black_pixels_proportion = nblack / float(len(pixels))
+    if black_pixels_proportion > threshold:
+        return True
+    else:
+        return False
+
+
+def is_similar(image_url_1, image_url_2, url: bool = False):
+    if url:
+        image_url_1 = requests.get(image_url_1, stream=True).raw
+        image_url_2 = requests.get(image_url_2, stream=True).raw
+    return image_url_1 == image_url_2
