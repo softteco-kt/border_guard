88957f4bc8003f2c53dcacb24102e189c6f74624
softteco-kt <k.turaev@softteco.com>
2023-02-15 16:41:32 +0500

    schema adj.

--- api/schemas.py
+++ api/schemas.py
@@ -10,7 +10,7 @@ from uuid import UUID
 
 class PeeweeGetterDict(GetterDict):
     """When you access a relationship in a Peewee object, like in some_user.items, Peewee doesn't provide a list of Item."""
-    
+
     def get(self, key: Any, default: Any = None):
         res = getattr(self._obj, key, default)
         if isinstance(res, peewee.ModelSelect):
@@ -25,10 +25,11 @@ class CarsMetaData(BaseModel):
 class BorderCaptureOut(BaseModel):
     id: UUID
     created_at: datetime.datetime
-    image_path: str 
+    image_path: str
     number_of_cars: int | None = None
     processed: bool | None = None
     processed_at: datetime.datetime | None = None
+    is_valid: bool | None = None
 
     class Config:
         orm_mode = True
