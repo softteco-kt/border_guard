cacb1ed7d6a201857a217a3b47647759d515826e
softteco-kt <k.turaev@softteco.com>
2023-02-20 13:31:21 +0500

    new filters, default time ranges

--- stats/custom_enums.py
+++ stats/custom_enums.py
@@ -16,11 +16,25 @@ class Timeframe(enum.StrEnum):
 class Columns(enum.StrEnum):
     """DataFrame column names"""
 
+    created_at = "created_at"
+    cars = "number_of_cars"
+    image_path = "image_path"
     moving_average = "Moving Average"
     image = "image"
-    image_path = "image_path"
-    cars = "Number of cars"
     time = "Datetime"
+    day_of_week = "Day of Week"
     day = "Day"
     hour = "Hour"
     minute = "Minute"
+
+
+class Weekdays(enum.IntEnum):
+    """Enum representing pandas int weekdays."""
+
+    monday = 0
+    tuesday = 1
+    wednesday = 2
+    thursday = 3
+    friday = 4
+    saturday = 5
+    sunday = 6
--- stats/main.py
+++ stats/main.py
@@ -31,17 +31,6 @@ input_date_to = to_widget.date_input("Date To:", dt.today() + td(days=1))
 
 
 try:
-    # Get API data
-    raw_data = get_data(
-        date_from=input_date_from,
-        date_to=input_date_to,
-    )
-
-    raw_data = add_url_column(raw_data)
-
-    # Include only valid data
-    raw_data = raw_data[raw_data.is_valid == True]
-
     sidebar_header = "**Additional params:**"
     match input_type:
 
@@ -52,6 +41,69 @@ try:
                 input_ma = st.number_input("Moving average:", 0)
                 input_tf = st.selectbox("Bucket by", Timeframe.list())
 
+                # Filtering inputs
+                with st.expander(label="Included days: ", expanded=False):
+                    include_monday = st.checkbox("Monday", value=True)
+                    include_tuesday = st.checkbox("Tuesday", value=True)
+                    include_wednesday = st.checkbox("Wednesday", value=True)
+                    include_thursday = st.checkbox("Thursday", value=True)
+                    include_friday = st.checkbox("Friday", value=True)
+                    include_saturday = st.checkbox("Saturday", value=True)
+                    include_sunday = st.checkbox("Sunday", value=True)
+
+                # Utility datetime ranges that overwrite initial values
+                input_last_n_days = st.selectbox(
+                    "Default datetime range: ",
+                    [None, 7, 14, 30],
+                    format_func=lambda x: f"Last {x} days." if x is not None else None,
+                )
+
+                if input_last_n_days:
+                    # Overwrite and disable initial date ranges
+                    input_date_from = dt.today() - td(days=input_last_n_days)
+                    input_date_to = dt.today() + td(days=1)
+
+            included_days = []
+            if include_monday:
+                included_days.append(Weekdays.monday)
+            if include_tuesday:
+                included_days.append(Weekdays.tuesday)
+            if include_wednesday:
+                included_days.append(Weekdays.wednesday)
+            if include_thursday:
+                included_days.append(Weekdays.thursday)
+            if include_friday:
+                included_days.append(Weekdays.friday)
+            if include_saturday:
+                included_days.append(Weekdays.saturday)
+            if include_sunday:
+                included_days.append(Weekdays.sunday)
+
+            # Get API data
+            raw_data = get_data(
+                date_from=input_date_from,
+                date_to=input_date_to,
+            )
+
+            raw_data = add_url_column(raw_data)
+
+            # Include only valid data
+            raw_data = raw_data[raw_data.is_valid == True]
+
+            # Convert created to datetime to use dt.day_of_week
+            raw_data[Columns.day_of_week] = pd.to_datetime(
+                raw_data.created_at
+            ).dt.day_of_week
+
+            # Filter raw data in pandas according to chosen checkboxes
+            raw_data["include"] = raw_data[Columns.day_of_week].apply(
+                lambda x: x in included_days
+            )
+
+            # Apply filter
+            raw_data = raw_data[raw_data.include == True]
+
+            # raw_data = raw_data[raw_data.]
             data = bin_data(
                 raw_data,
                 timeframe=input_tf,
@@ -69,6 +121,17 @@ try:
                     "Aggregate by", [Timeframe.hourly, Timeframe.daily]
                 )
 
+            # Get API data
+            raw_data = get_data(
+                date_from=input_date_from,
+                date_to=input_date_to,
+            )
+
+            raw_data = add_url_column(raw_data)
+
+            # Include only valid data
+            raw_data = raw_data[raw_data.is_valid == True]
+
             # Apply aggregation to data
             data = agg_data(
                 raw_data,
@@ -79,4 +142,5 @@ try:
     st.altair_chart(chart, use_container_width=True)
     st.write("*\* Time is in UTC*")
 except Exception as e:
+    e
     st.write("No data...")
