1d70263bbd04a6773c67e998ea88dc2d5d9fbc4e
softteco-kt <k.turaev@softteco.com>
2023-02-27 20:47:12 +0500

    task retry behaviour

--- worker/worker/tasks.py
+++ worker/worker/tasks.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import sys
+import traceback
 
 import requests
 import celery
@@ -9,35 +10,53 @@ from models import BorderCapture
 from models import database as database_connection
 from worker.main import app
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
     "%(asctime)s | %(levelname)s | %(message)s", "%m-%d-%Y %H:%M:%S"
 )
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+# logger.setFormatter(formatter)
 
-stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setLevel(logging.INFO)
-stdout_handler.setFormatter(formatter)
+# stdout_handler = logging.StreamHandler(sys.stdout)
+# stdout_handler.setLevel(logging.INFO)
+# stdout_handler.setFormatter(formatter)
 
-logger.addHandler(stdout_handler)
+# logger.addHandler(stdout_handler)
 
 
 class LogErrorsTask(celery.Task):
+    autoretry_for = (Exception,)
+    retry_kwargs = {"max_retries": 5, "interval_start": 3}
+    retry_backoff = True
+
+    def on_retry(self, exc, task_id, args, kwargs, einfo):
+        logger.info(
+            f"Task {task_id} with args: {args} failed. Retries left, {self.max_retries - self.request.retries}"
+        )
+        # logger.exception(f"Task failed with exception: {exc}")
+        return super().on_retry(exc, task_id, args, kwargs, einfo)
+
     def on_failure(self, exc, task_id, args, kwargs, einfo):
-        logger.info("{0!r} failed: {1!r}".format(task_id, exc))
-        # logger.exception("Celery task failed.", exc_info=exc)
-        super(LogErrorsTask, self).on_failure(exc, task_id, args, kwargs, einfo)
+        logger.info(f"{task_id} failed: {exc}")
+        super().on_failure(exc, task_id, args, kwargs, einfo)
 
 
-@app.task(base=LogErrorsTask, ack_late=True)
-def process_img(image_id):
+@app.task(base=LogErrorsTask, bind=True, ack_late=True, ignore_result=True)
+def process_img(self, image_id):
     # init connection and automatically close when task is completed
     with database_connection:
 
-        # Check if retrieved image is valid
-        image_is_valid = requests.post(
-            "http://api:8000/is_valid", data={"image_id": image_id}
-        )
+        try:
+            # Check if retrieved image is valid
+            image_is_valid = requests.post(
+                "http://api:8000/is_valid",
+                data={"image_id": image_id},
+                timeout=(5, 30),
+            )
+            assert image_is_valid.status_code == 200
+        except Exception as e:
+            logger.error(traceback.format_exc(limit=1))
+            raise Exception("API probably not accessible")
 
         if image_is_valid:
 
